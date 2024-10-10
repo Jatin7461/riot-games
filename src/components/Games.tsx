d8e402aca3d5c0c849a48fbdcae8b68cda74017d
Jatin Gandhi <goldcoinjatin@gmail.com>
2024-10-10 16:27:08 +0530

    removed gsap trial

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -3,15 +3,24 @@ import SplitText from "gsap-trial/SplitText"
 import { useGSAP } from "@gsap/react"
 import { RefObject, useRef } from "react"
 import React from "react"
+import { motion } from 'framer-motion'
 
 const Games = ({ heading }: { heading: string }) => {
 
 
+    let text = heading.split("")
+    console.log(text)
 
     return <div
         className="games ">
         {/* <img className="riot-logo" src="riot-circle.png" alt="" /> */}
-        <h1 className="name">{heading}</h1>
+        <h1 className="name">{text.map((s, i) => {
+            return <span className={`letter letter-${i}`} key={i}
+                // initial={{ y: 100, opacity: 0 }}
+                // animate={{ y: 0, opacity: 1, }}
+                // transition={{ duration: 0.5, delay: (6 + i) * 0.1 }}
+            >{s}</span>
+        })}</h1>
     </div>
 }
 
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,10 +3,7 @@ import Header from './Header';
 import GameCard from './GameCard';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
-import SplitText from 'gsap-trial/SplitText';
 import Observer from 'gsap/Observer';
-import { Outlet } from 'react-router-dom';
-import Transition from './Transition';
 import { RefObject } from 'react';
 import React from 'react';
 
@@ -36,87 +33,50 @@ const HomePage = () => {
 
     useGSAP(() => {
 
-
-        gsap.registerPlugin(SplitText)
-        let split = new SplitText('.name', {
-            type: 'words,chars'
+        gsap.from('.game-card', {
+            y: 100,
+            opacity: 0,
+            stagger: 0.2,
+            delay: 1.5
         })
-        let chars = split.chars;
-        console.log(chars)
+
         gsap.timeline()
-            // .to('.riot-logo', {
-            //     x: 100,
-            //     rotation: 90,
-            //     opacity: 0,
-            //     delay: 1
-            // })
             .from('.stag', {
                 y: -100,
                 opacity: 0,
                 stagger: 0.1,
-                // duration: 0.1
             })
-            .from(chars, {
+            .from('.letter', {
                 y: 100,
                 opacity: 0,
                 stagger: 0.1
             })
-
-            // .to('.name', {
-            //   y: -100,
-            //   scale: 0.8
+            // .from('.game-card', {
+            //     y: 100,
+            //     opacity: 0,
+            //     stagger: 0.2,
+            //     delay: 0.2
             // })
-            .from('.game-card', {
-                y: 100,
-                opacity: 0,
-                stagger: 0.2,
-            })
-            // .to(chars[1], {
-            //     rotation: 180,
-            //     color: 'red',
-            //     duration: 0.2
-            // })
-            .to(chars[8], {
+            .to('.letter-9', {
                 rotation: 180,
                 color: 'red',
                 duration: 0.2
             })
-            .to(chars[2], {
+            .to('.letter-2', {
                 rotation: 180,
                 color: 'red',
                 duration: 0.2
             })
-            .to(chars[7], {
+            .to('.letter-8', {
                 rotateX: 180,
                 color: 'red',
                 duration: 0.2
             })
-            .to(chars[3], {
+            .to('.letter-3', {
                 rotateY: 180,
                 color: 'red',
                 duration: 0.2
             })
-            // .to('.lol', {
-            //     scale: 1.05,
-            //     duration: 0.1
-            // })
-            // .to('.lol', {
-            //     scale: 1
-            // })
-            // .to('.valo', {
-            //     scale: 1.05,
-            //     duration: 0.1
-            // })
-            // .to('.valo', {
-            //     scale: 1
-            // })
-            // .to('.tft', {
-            //     scale: 1.05,
-            //     duration: 0.1
-            // })
-            // .to('.tft', {
-            //     scale: 1
-            // })
             .to(['.lor', '.tft', '.lol', '.valo'], {
                 delay: 0.5,
                 scale: 1.05,
@@ -125,30 +85,10 @@ const HomePage = () => {
             .to(['.lor', '.tft', '.lol', '.valo'], {
                 scale: 1
             })
-        // .to(chars[1], {
-        //     rotation: 180,
-        //     duration: 2,
-        //     delay: 1
-        // })
-        // .to(chars[2], {
-        //     rotation: 180,
-        //     duration: 0.2
-        //     , delay: 5
-        // })
-        // .to(chars[8], {
-        //     rotation: 180,
-        //     duration: 0.2
-        //     , delay: 5
-        // })
-        // .to(chars[1], {
-        //     rotation: 180,
-        //     duration: 0.2
-        //     , delay: 5
-        // })
     }, { scope: container })
     return <div ref={container} className='flex flex-col h-full'>
         <Header />
-        <Games heading="RIOT GAMES"/>
+        <Games heading="RIOT GAMES" />
         <div className='flex gap-2 m-4'>
 
             <GameCard a='lol' cb={scaleFunction} />
